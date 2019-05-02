60e11cff77adc59158d0a3d3b36b7fdb4f419259
bhy304 <bhy0512@gmail.com>
2019-05-02 09:41:49 +0900

    DB 생성

--- tts/admin.py
+++ tts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Maratron
 
 # Register your models here.
+
+# admin.site.register(Maratron)
+
+@admin.register(Maratron)
+class MaratronAdmin(admin.ModelAdmin):
+    list_display = ['index', 'category', 'title', 'name'] # 보여줄 칼럼
+    list_display_links = ['index', 'category', 'title', 'name'] # 상세보기 링크 설정
+    list_filter = ["index"] # filter 기준 칼럼
+    search_fields = ["category", 'name'] # 검색 컬럼
